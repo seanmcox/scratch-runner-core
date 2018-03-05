c2f40e9086d82411ac4c15284a2a7920b764a35a
seanmcox <github@smcox.com>
2018-03-05 11:14:03 -0700

    Added script cloning to ScriptTuple interface.

--- src/com/shtick/utils/scratch/runner/core/elements/ScriptTuple.java
+++ src/com/shtick/utils/scratch/runner/core/elements/ScriptTuple.java
@@ -33,4 +33,16 @@ public interface ScriptTuple extends Tuple{
 	 * @return the context
 	 */
 	public ScriptContext getContext();
+	
+	/**
+	 * When creating a sprite clone, exactly the same script will be associated with the new clone,
+	 * just with a different context.
+	 * 
+	 * TODO Come up with a pithy name that isn't easily confused with an existing standard function name.
+	 * 
+	 * @param context
+	 * @return A ScriptTupleImplementation that contains the same script with given context.
+	 */
+	public ScriptTuple clone(ScriptContext context);
+
 }
