b41403eb99ffe669833d192a2ec5eaa8459df81a
Sean M. Cox <github@smcox.com>
2018-06-10 23:16:51 -0600

    Added new main interface.

---
+++ src/com/shtick/utils/scratch/runner/core/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.shtick.utils.scratch.runner.core;
+
+import java.io.IOException;
+
+/**
+ * @author scox
+ *
+ */
+public interface Main {
+	/**
+	 * 
+	 * @param args
+	 * @throws Throwable 
+\	 */
+	public void main(String[] args) throws Throwable;
+}
